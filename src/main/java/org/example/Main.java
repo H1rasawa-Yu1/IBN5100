f7772929cebcef206275ebcf1d84db774d54adfb
H1rasawa-Yu1 <2198965582@qq.com>
2025-04-22 15:40:05 +0800

    接口学习

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,32 @@
 package org.example;
-abstract class Animal{
-    abstract void shout();
-}
-class Dog extends Animal{
-    void shout(){
-        System.out.printf("wolf");
+interface Animal{
+    int age=1;
+    String name="cz";
+    void shout();
+    public void info();
+    static int getAge() {
+        return age;
     }
 }
-public class Main{
-    public static void main(String[] args){
-        Dog dog=new Dog();
+interface Action{
+    public void eat();
+}
+class Dog implements Animal,Action{
+    public void eat(){
+        System.out.println("dog eat");
+    }
+    public void info(){
+        System.out.println("dog info");
+    }
+    public void shout(){
+        System.out.println("dog shout");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.info();
+        dog.eat();
         dog.shout();
     }
 }
