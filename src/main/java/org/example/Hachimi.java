b5d98219cef20fd4fe17e65d943f2fa1af6dfd8a
H1rasawa-Yu1 <2198965582@qq.com>
2025-04-28 19:01:30 +0800

    接口（注：接口中的方法不能具体定义只能声明，否则会报错）

---
+++ src/main/java/org/example/Hachimi.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Hachimi implements doA,doB{
+     public void sayHello(){
+         System.out.println("Hello");
+     }
+     public void eat(){
+         System.out.println("Eat");
+     }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 public class Main{
     public static void main(String [] args){
-        Husband hs=new Husband();
-        hs.setAge(17);
-        hs.setName("Husband");
-        System.out.println(hs.getAge());
+       Hachimi ha=new Hachimi();
+      ha.eat();
+      ha.sayHello();
     }
 }
