a225a353b17a82908b34df1089108076749e4f9b
H1rasawa-Yu1 <2198965582@qq.com>
2025-04-28 18:45:37 +0800

    封装

---
+++ src/main/java/org/example/Husband.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Husband {
+    private int age;
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+}
