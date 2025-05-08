3402163438129f04dcfddc8bff04d3ffab3b9f95
H1rasawa-Yu1 <2198965582@qq.com>
2025-05-08 17:37:58 +0800

    静态方法（静态方法属于类而不是对象，所以访问时要从类访问）（静态方法能且只能访问静态变量）

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,12 +1,29 @@
 package org.example;
 public class test{
-   static int num=0;
-   test(){
-       num++;
-       System.out.println(num);
-   }
-   public static void main(String []args){
-       test t1=new test();
-       test t2=new test();
-   }
+    String name;
+    int age;
+    static String sch="zut";
+    test(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    String getName(){
+        return name;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+    static void change(){
+        sch="lab";
+    }
+    void out(){
+        System.out.println(name+","+age+","+sch);
+    }
+    public static void main(String[] args){
+        test.change();
+        test t1=new test("yui",18);
+        test t2=new test("saki",18);
+        t1.out();
+        t2.out();
+    }
 }
