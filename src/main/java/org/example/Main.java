b8a5d6981b9bd77a5c1854a47d9fa9f970d80fb6
H1rasawa-Yu1 <2198965582@qq.com>
2025-05-08 16:54:47 +0800

    静态变量

--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("yui",18);
-        System.out.println(person.age);
-    }
-}
