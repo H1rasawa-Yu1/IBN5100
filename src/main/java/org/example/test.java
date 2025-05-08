c85b06cd8da801ed42103d008e9397b62f487537
H1rasawa-Yu1 <2198965582@qq.com>
2025-05-08 17:00:21 +0800

    静态变量

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,24 +1,12 @@
 package org.example;
 public class test{
-    String name;
-    int age;
-    static String sch="zut";
-    public test(String name,int age){
-        this.name=name;
-        this.age=age;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-}
-class Main{
-    public static void main(String []args){
-        test t1=new test("yui",18);
-        test t2=new test("saki",18);
-        System.out.println(t1.getName());
-        System.out.println(test.sch);
-    }
+   int num=0;
+   test(){
+       num++;
+       System.out.println(num);
+   }
+   public static void main(String []args){
+       test t1=new test();
+       test t2=new test();
+   }
 }
